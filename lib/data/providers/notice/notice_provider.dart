0629d18c46df1501018ed6164c7314baac1e954c
leewoooo <leewoooo@visionerp.org>
2023-02-17 17:02:35 +0900

    ✨Add: notice provider & repository

---
+++ lib/data/providers/notice/notice_provider.dart
@@ -0,0 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:pyc/data/clients/client_interface.dart';
+import 'package:pyc/data/models/notice/requests/create_notice_request.dart';
+import 'package:pyc/data/providers/notice/notice_provider_interface.dart';
+
+class NoticeProvider implements INoticeProvider {
+  final IClient<Dio> client;
+  NoticeProvider(this.client);
+
+  @override
+  Future<void> write(String title, String content) async {
+    final authClient = await client.getAuthClient();
+    await authClient.post(
+      '/notices',
+      data: CreateNoticeRequest(title, content).toJSON(),
+    );
+  }
+
+  @override
+  Future<Response> findAll({int offset = 0, int limit = 20}) async {
+    final authClient = await client.getAuthClient();
+    return authClient.get(
+      '/notices',
+      queryParameters: {"offset": offset, "limit": limit},
+    );
+  }
+
+  @override
+  Future<Response> findById(int id) async {
+    final authClient = await client.getAuthClient();
+    return authClient.get('/notices/$id');
+  }
+
+  @override
+  Future<void> modify(String title, String content) async {
+    final authClient = await client.getAuthClient();
+    await authClient.post(
+      '/notices',
+      data: CreateNoticeRequest(title, content).toJSON(),
+    );
+  }
+
+  @override
+  Future<void> deleteById(int id) async {
+    final authClient = await client.getAuthClient();
+    await authClient.delete('/notices/$id');
+  }
+}
---
+++ lib/data/providers/notice/notice_provider_interface.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+abstract class INoticeProvider {
+  // C
+  Future<void> write(String title, String content);
+
+  // R
+  Future<Response> findById(int id);
+  Future<Response> findAll({int offset, int limit});
+
+  // U
+  Future<void> modify(String title, String content);
+
+  // D
+  Future<void> deleteById(int id);
+}

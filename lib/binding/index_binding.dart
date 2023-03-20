11f7106fab50cdfcf712beea267822dbfac7f7b8
leewoooo <leewoooo.dev@gmail.com>
2023-03-20 23:45:15 +0900

    🔨Modify: change calendar controller (index)

--- lib/binding/index_binding.dart
+++ lib/binding/index_binding.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
-import 'package:pyc/controllers/calendar/calendar_controller.dart';
+import 'package:pyc/controllers/calendar/index_calendar_controller.dart';
 import 'package:pyc/controllers/index/fetch_me_controller.dart';
 import 'package:pyc/controllers/notice/notice_controller.dart';
 import 'package:pyc/data/clients/client.dart';
+import 'package:pyc/data/providers/calendar/calendar_provider.dart';
 import 'package:pyc/data/providers/notice/notice_provider.dart';
 import 'package:pyc/data/providers/user/user_provider.dart';
 import 'package:pyc/data/repositories/auth/auth_repository.dart';
+import 'package:pyc/data/repositories/calendar/calendar_repository.dart';
 import 'package:pyc/data/repositories/notice/notice_repository.dart';
 import 'package:pyc/data/repositories/user/user_repository.dart';
 
@@ -16,9 +18,10 @@ class IndexBinding extends Bindings {
     final authRepository = Get.find<AuthRepository>();
     final userRepository = Get.put<UserRepository>(UserRepository(UserProvider(client)));
     final noticeRepository = Get.put<NoticeRepository>(NoticeRepository(NoticeProvider(client)));
+    final calendarRepository = Get.put<CalendarRepository>(CalendarRepository(CalendarProvider()));
 
     Get.put<FetchMeController>(FetchMeController(userRepository, authRepository));
     Get.put<NoticeController>(NoticeController(noticeRepository));
-    Get.put<CalendarController>(CalendarController());
+    Get.put<IndexCalendarController>(IndexCalendarController(calendarRepository));
   }
 }
